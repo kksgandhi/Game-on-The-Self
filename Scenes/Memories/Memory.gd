6578eeaba92fe348281eb610c93efa18779957e3
Kutub Gandhi <kksgandhi@gmail.com>
2022-11-25 21:16:21 -0500

    enlarge memory when active

--- Scenes/Memories/Memory.gd
+++ Scenes/Memories/Memory.gd
@@ -6,9 +6,11 @@ var isMemoryActive = false
 
 func _on_Memory_body_entered(_body):
   isMemoryActive = true
+  $Sprite.scale = Vector2(0.5, 0.5)
 
 func _on_Memory_body_exited(_body):
   isMemoryActive = false
+  $Sprite.scale = Vector2(0.3, 0.3)
 
 func _process(_delta):
   if isMemoryActive and Input.is_action_just_pressed("ui_accept"):
