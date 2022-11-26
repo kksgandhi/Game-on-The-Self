2aed6c333a09a5d6beb1dee12b71b6ca772203a8
Kutub Gandhi <kksgandhi@gmail.com>
2022-11-25 21:12:51 -0500

    add multiple memories that can respond to input

--- Scenes/Memories/Memory.gd
+++ Scenes/Memories/Memory.gd
@@ -1,5 +1,15 @@
 extends Area2D
 
-func _on_Memory_body_entered(body):
-  body.enter_memory()
-	pass
+export var scene = ""
+
+var isMemoryActive = false
+
+func _on_Memory_body_entered(_body):
+  isMemoryActive = true
+
+func _on_Memory_body_exited(_body):
+  isMemoryActive = false
+
+func _process(_delta):
+  if isMemoryActive and Input.is_action_just_pressed("ui_accept"):
+    print(scene)
